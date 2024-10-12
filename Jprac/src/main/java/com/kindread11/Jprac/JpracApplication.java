ad1032b0cc39ba551641e693e5739e32355db959
kindread11 <boris9605@naver.com>
2024-10-12 17:51:49 +0900

    241012 feat : 프로젝트 추가

---
+++ Jprac/src/main/java/com/kindread11/Jprac/JpracApplication.java
@@ -0,0 +1,13 @@
+package com.kindread11.Jprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpracApplication.class, args);
+	}
+
+}
---
+++ Jprac/src/test/java/com/kindread11/Jprac/JpracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kindread11.Jprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
